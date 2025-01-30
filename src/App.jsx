8e69b1498e775a4de031983841193817626f7aa8
Tom <tom.d.keogh+code@gmail.com>
2025-01-30 21:02:44 +0000

    steal title from last project

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import Goals from './GoalsPage.jsx'
 import Success from './SuccessPage.jsx'
 import Result from './ResultPage.jsx'
 
-
-
 function App() {
 
   const [title, setTitle] = useState("This is the title")
@@ -17,6 +15,9 @@ function App() {
   return (
 
     <div class='mainDiv bg-regal-blue w-full h-screen'>
+      <p class="p-10 text-6xl font-bold tracking-tight bg-transparent flex flex-row justify-center">
+        Create PRD Templates using  <p class="pl-4 bg-transparent font-extrabold bg-gradient-to-bl  from-blue-600 via-teal-500 to-amber-400  bg-clip-text text-transparent "> GenAI</p>
+      </p>
       <div class='titleDiv flex flex-row justify-center items-center '>
         <p>{title}</p>
       </div>
@@ -30,35 +31,35 @@ function App() {
           onClick={e => openTab(e, 'tab2')}>Tab 2</button>
         <button class="tabButton  w-full py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none hover:bg-gray-400"
           onClick={e => openTab(e, 'tab3')}>Tab 3</button>
-          <button class="tabButton  w-full py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none hover:bg-gray-400"
+        <button class="tabButton  w-full py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none hover:bg-gray-400"
           onClick={e => openTab(e, 'tab4')}>Tab 4</button>
       </div>
       <div id="tab1" class="tabcontent p-4 w-full grid grid-cols-1 gap-4 justify-center items-center">
         <Summary setNewTitle={replaceTitle} setNewTitle2={replacetwo} />
       </div>
       <div id="tab2" class="tabcontent p-4 w-full flex flex-row justify-center items-center hidden">
-        <Goals/>
+        <Goals />
       </div>
       <div id="tab3" class="tabcontent p-4 w-full flex flex-row justify-center items-center hidden">
-        <Success/>
+        <Success />
       </div>
       <div id="tab4" class="tabcontent p-4 w-full flex flex-row justify-center items-center hidden">
-        <Result/>
+        <Result />
       </div>
     </div>
 
   )
 
-  function replaceTitle(){
+  function replaceTitle() {
     console.log("button clicked")
     setTitle("first prop")
   }
-  function replacetwo(){
+  function replacetwo() {
     setTitle("second clicked")
   }
 
   function openTab(evt, tabName) {
-    
+
     console.log("opentab")
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
