28d0f55235f545c62d2eaf633758a6a2077626eb
Tom <tom.d.keogh+code@gmail.com>
2025-01-31 17:28:29 +0000

    Add tab styling with fade and colours

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,32 @@ import Result from './ResultPage.jsx'
 
 function App() {
 
-  const [title, setTitle] = useState("This is the title")
+  const [title, setTitle] = useState("Passing state example")
 
 
   return (
 
     <div class='mainDiv bg-regal-blue w-full h-screen'>
-      <p class="p-10 text-6xl font-bold tracking-tight bg-transparent flex flex-row justify-center">
-        Create PRD Templates using  <p class="pl-4 bg-transparent font-extrabold bg-gradient-to-bl  from-blue-600 via-teal-500 to-amber-400  bg-clip-text text-transparent "> GenAI</p>
+      <p class="p-10 text-6xl font-bold tracking-tight bg-transparent flex flex-row text-center justify-center">
+         <p class="pl-4 bg-transparent font-extrabold bg-gradient-to-bl  from-blue-600 via-teal-500 to-amber-400  bg-clip-text text-transparent ">Create PRD Templates using GenAI</p>
       </p>
       <div class='titleDiv flex flex-row justify-center items-center '>
         <p>{title}</p>
       </div>
       <br />
-      <div class="w-full flex">
-        <button
-          class="tabButton w-full py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none active:bg-gray-200 hover:bg-gray-400"
-          onClick={e => openTab(e, 'tab1')}>Tab 1</button>
-        <br />
-        <button class="tabButton  w-full py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none hover:bg-gray-400"
-          onClick={e => openTab(e, 'tab2')}>Tab 2</button>
-        <button class="tabButton  w-full py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none hover:bg-gray-400"
-          onClick={e => openTab(e, 'tab3')}>Tab 3</button>
-        <button class="tabButton  w-full py-4 text-center font-medium text-gray-700 bg-gray-100 rounded-lg focus:outline-none hover:bg-gray-400"
-          onClick={e => openTab(e, 'tab4')}>Tab 4</button>
+      <div class="flex flex-row justify-center">
+        <div class="w-5/6 flex relative flex flex-row gap-1 px-1.5 py-1 list-none rounded-md bg-slate-500">
+          <button
+            class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab1')}>Tab 1</button>
+          <br />
+          <button class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab2')}>Tab 2</button>
+          <button class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab3')}>Tab 3</button>
+          <button class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab4')}>Tab 4</button>
+        </div>
       </div>
       <div id="tab1" class="tabcontent p-4 w-full grid grid-cols-1 gap-4 justify-center items-center">
         <Summary setNewTitle={replaceTitle} setNewTitle2={replacetwo} />
--- src/GoalsPage.jsx
+++ src/GoalsPage.jsx
@@ -4,7 +4,9 @@ import { useState } from 'react'
 export default function App() {
 
     return (
-        <div>Hello Two</div>
+        <div class = 'flex flex-column w-5/6 justify-center text-center'> 
+            <p>This page is for adding your goals, you can assign goals to specific user personas. We suggest just a 5-6 high level goals spread across 2-3 user personas.</p>
+        </div>
     )
 
 }
