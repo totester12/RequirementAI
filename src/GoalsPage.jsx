91c80a793357aced8c2300dff9a8dc928c8edc3f
Tom <tom.d.keogh+code@gmail.com>
2025-01-31 17:50:04 +0000

    nesting

--- src/GoalsPage.jsx
+++ src/GoalsPage.jsx
@@ -4,9 +4,15 @@ import { useState } from 'react'
 export default function App() {
 
     return (
+        <div class = "w-5/6 justify-center text-center items-center flex flex-col">
         <div class = 'flex flex-column w-5/6 justify-center text-center'> 
             <p>This page is for adding your goals, you can assign goals to specific user personas. We suggest just a 5-6 high level goals spread across 2-3 user personas.</p>
+            
+        </div>
+        <p>test</p>
+        
         </div>
     )
+    /* look at nested flex containers to handle layout*/
 
 }
