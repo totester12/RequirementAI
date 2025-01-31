c743219a4988a19d3fb87d807cb82c7c85fa9a98
Tom <tom.d.keogh+code@gmail.com>
2025-01-31 17:43:27 +0000

    sort padding, add radial bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,35 @@ import Success from './SuccessPage.jsx'
 import Result from './ResultPage.jsx'
 
 function App() {
+  
 
   const [title, setTitle] = useState("Passing state example")
 
 
   return (
 
-    <div class='mainDiv bg-regal-blue w-full h-screen'>
-      <p class="p-10 text-6xl font-bold tracking-tight bg-transparent flex flex-row text-center justify-center">
-         <p class="pl-4 bg-transparent font-extrabold bg-gradient-to-bl  from-blue-600 via-teal-500 to-amber-400  bg-clip-text text-transparent ">Create PRD Templates using GenAI</p>
-      </p>
+    <div class='mainDiv absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_70%_80%_at_50%_-10%,rgba(10,119,198,0.3),rgba(255,255,255,0))]  '>
+      <div >
+        <p class="pt-8 pb-3 text-6xl font-bold tracking-tight bg-transparent flex flex-row text-center justify-center">
+          <p class=" bg-transparent font-extrabold bg-gradient-to-bl  from-blue-600 via-teal-500 to-amber-400  bg-clip-text text-transparent ">Create PRD Templates with GenAI</p>
+        </p>
+      </div>
       <div class='titleDiv flex flex-row justify-center items-center '>
-        <p>{title}</p>
+        <p class="hidden">{title}</p>
       </div>
       <br />
       <div class="flex flex-row justify-center">
         <div class="w-5/6 flex relative flex flex-row gap-1 px-1.5 py-1 list-none rounded-md bg-slate-500">
           <button
-            class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
-            onClick={e => openTab(e, 'tab1')}>Tab 1</button>
+            class="tabButton duration-400 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab1')}>Summary</button>
           <br />
-          <button class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
-            onClick={e => openTab(e, 'tab2')}>Tab 2</button>
-          <button class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
-            onClick={e => openTab(e, 'tab3')}>Tab 3</button>
-          <button class="tabButton duration-300 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
-            onClick={e => openTab(e, 'tab4')}>Tab 4</button>
+          <button class="tabButton duration-400 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab2')}>Goals</button>
+          <button class="tabButton duration-400 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab3')}>Success</button>
+          <button class="tabButton duration-400 transition-all ease-in-out w-5/6 p-4 text-center font-medium text-gray-700  rounded-lg focus:outline-none focus:bg-blue-200 hover:bg-slate-400"
+            onClick={e => openTab(e, 'tab4')}>Result</button>
         </div>
       </div>
       <div id="tab1" class="tabcontent p-4 w-full grid grid-cols-1 gap-4 justify-center items-center">
